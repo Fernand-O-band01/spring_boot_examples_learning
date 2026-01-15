2d3f44c572c725ec986d9d362dbd95b8bbe6ff45
Fernand-O-band01 <112786231+Fernand-O-band01@users.noreply.github.com>
2026-01-14 22:29:27 -0600

    initializing

---
+++ src/main/java/com/example/fer/example_v2/ExampleV2Application.java
@@ -0,0 +1,13 @@
+package com.example.fer.example_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fer/example_v2/ExampleV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fer.example_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
